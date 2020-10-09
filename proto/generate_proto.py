f7200154592e06ec74ff2349d20585ab2871c034
goagain <remain.goagain@gmail.com>
2020-10-09 17:03:44 -0400

    simple search

--- proto/generate_proto.py
+++ proto/generate_proto.py
@@ -11,3 +11,5 @@ grpc_tools.main([sys.argv[0], "--python_out=../server",
                  "--grpc_python_out=../server", "-I.", "./game.proto"])
 grpc_tools.main([sys.argv[0], "--python_out=../client",
                  "--grpc_python_out=../client", "-I.", "./game.proto"])
+grpc_tools.main([sys.argv[0], "--python_out=../simple search",
+                 "--grpc_python_out=../simple search", "-I.", "./game.proto"])
